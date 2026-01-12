f2b274a6a681ee8cbd472cce68d6e7588c3227f1
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-01-12 10:21:56 +0800

    Update Main.cpp

--- RocketJumper/Main.cpp
+++ RocketJumper/Main.cpp
@@ -25,7 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// Initialization of your own variables go here
 
 	// Using custom window procedure
-	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 60, false, NULL);
+	AESysInit(hInstance, nCmdShow, 1000, 900, 1, 60, false, NULL);
 
 	// Changing the window title
 	AESysSetWindowTitle("My New Demo!");
