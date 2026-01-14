e7f1ece3b0dec9736fa8b837b006f1257b43b511
i.chong <i.chong@digipen.edu>
2026-01-14 09:39:34 +0800

    main with stuff

--- RocketJumper/Main.cpp
+++ RocketJumper/Main.cpp
@@ -3,11 +3,37 @@
 
 #include <crtdbg.h> // To check for memory leaks
 #include "AEEngine.h"
+#include "collision.h"
 
 
-// jo test
+
 // ---------------------------------------------------------------------------
 // main
+
+
+
+
+
+namespace renderlogic {
+	void Drawsquare(f32 xpos, f32 ypos, f32 xsize, f32 ysize) {
+		AEMtx33 scale = { 0 };
+		AEMtx33Scale(&scale, xsize, ysize);
+
+		AEMtx33 rotate = { 0 };
+		AEMtx33Rot(&rotate, 0.0f);
+
+		AEMtx33 translate = { 0 };
+		AEMtx33Trans(&translate, xpos, ypos);
+
+		AEMtx33 transform = { 0 };
+		AEMtx33Concat(&transform, &rotate, &scale);
+		AEMtx33Concat(&transform, &translate, &transform);
+
+		AEGfxSetTransform(transform.m);
+
+	}
+}
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -23,6 +49,19 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	int gGameRunning = 1;
 
 	// Initialization of your own variables go here
+	
+	s8 speed = 10;
+	objectsquares objectinfo[2] = { 0 };
+
+	objectinfo[player].xpos = 0.0f;
+	objectinfo[player].ypos = 0.0f;
+	objectinfo[player].xscale = 100.0f;
+	objectinfo[player].yscale = 100.0f;
+
+	objectinfo[obstacle].xpos = -400.0f;
+	objectinfo[obstacle].ypos = 0.0f;
+	objectinfo[obstacle].xscale = 100.0f;
+	objectinfo[obstacle].yscale = 400.0f;
 
 	// Using custom window procedure
 	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 60, false, NULL);
@@ -32,32 +71,89 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	// reset the system modules
 	AESysReset();
+	AEGfxMeshStart();
 
-	printf("Hello World\n");
+	AEGfxTriAdd(
+		-0.5f, -0.5f, 0xFF000000, 0.0f, 1.0f,
+		0.5f, -0.5f, 0xFF000000, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0xFF000000, 0.0f, 0.0f);
+
+	AEGfxTriAdd(
+		0.5f, -0.5f, 0xFF000000, 1.0f, 1.0f,
+		0.5f, 0.5f, 0xFF000000, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0xFF000000, 0.0f, 0.0f);
+
+	AEGfxVertexList* pMesh = AEGfxMeshEnd();
+
+	AEGfxTriAdd(
+		0.0f, -0.5f, 0xFF000000, 0.0f, 1.0f,
+		1.0f, -0.5f, 0xFF000000, 1.0f, 1.0f,
+		0.0f, 0.5f, 0xFF000000, 0.0f, 0.0f);
+
+	AEGfxTriAdd(
+		1.0f, -0.5f, 0xFF000000, 1.0f, 1.0f,
+		1.0f, 0.5f, 0xFF000000, 1.0f, 0.0f,
+		0.0f, 0.5f, 0xFF000000, 0.0f, 0.0f);
+
+	AEGfxVertexList* HMesh = AEGfxMeshEnd();
+
 
 	// Game Loop
 	while (gGameRunning)
 	{
 		// Informing the system about the loop's start
 		AESysFrameStart();
-
 		// Basic way to trigger exiting the application
 		// when ESCAPE is hit or when the window is closed
 		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
 			gGameRunning = 0;
 
+		if (AEInputCheckCurr(AEVK_D)) {
+			objectinfo[player].xpos += static_cast<f32>(speed);
+		}
+
+		if (AEInputCheckCurr(AEVK_A)) {
+			objectinfo[player].xpos -= static_cast<f32>(speed);
+		}
+
+		if (AEInputCheckCurr(AEVK_W)) {
+			objectinfo[player].ypos += static_cast<f32>(speed);
+		}
+
+		if (AEInputCheckCurr(AEVK_S)) {
+			objectinfo[player].ypos -= static_cast<f32>(speed);
+		}
 		// Your own update logic goes here
 
+		if (gamelogic::collision(&objectinfo[player], &objectinfo[obstacle])) {
+			printf("collision");
+		}
 
 		// Your own rendering logic goes here
+		AEGfxSetBackgroundColor(0.5f, 0.5f, 0.5f);
 
+		AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+
+		AEGfxSetColorToMultiply(1.0f, 1.0f, 1.0f, 1.0f);
+
+		AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+		AEGfxSetTransparency(1.0f);
+
+		AEGfxSetColorToAdd(1.0f, 0.0f, 0.0f, 0.0f);
+		renderlogic::Drawsquare(objectinfo[obstacle].xpos, objectinfo[obstacle].ypos, objectinfo[obstacle].xscale, objectinfo[obstacle].yscale);
+		AEGfxMeshDraw(pMesh, AE_GFX_MDM_TRIANGLES);
+
+		AEGfxSetColorToAdd(1.0f, 1.0f, 1.0f, 1.0f);
+		renderlogic::Drawsquare(objectinfo[player].xpos, objectinfo[player].ypos, objectinfo[player].xscale, objectinfo[player].yscale);
+		AEGfxMeshDraw(pMesh, AE_GFX_MDM_TRIANGLES);
 
 		// Informing the system about the loop's end
 		AESysFrameEnd();
 
 	}
 
-
+	AEGfxMeshFree(HMesh);
+	AEGfxMeshFree(pMesh);
 	// free the system
 	AESysExit();
 }
