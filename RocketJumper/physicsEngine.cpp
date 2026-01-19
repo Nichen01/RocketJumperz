3b7b3c392cac8079a56c14c6b0507d9066116096
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-01-19 16:31:39 +0800

    update files

---
+++ RocketJumper/physicsEngine.cpp
@@ -0,0 +1,14 @@
+/* Start Header ************************************************************************/
+/*!
+\file		  physicsEngine.cpp
+\author       Jeremiah He Shuo, jeremiahshuo.he,2400702
+\par          jeremiahshuo.he@digipen.edu
+\date         January, 19, 2026
+\brief        Contains functions to simulate physics
+
+Copyright (C) 2026 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
