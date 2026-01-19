8fe344555792c13e08dffb19d77550c0fc12312a
cjoraye <cjoraye@gmail.com>
2026-01-19 09:25:34 +0800

    Added assets

--- RocketJumper/Main.cpp
+++ RocketJumper/Main.cpp
@@ -2,7 +2,7 @@
 // includes
 
 #include <crtdbg.h> // To check for memory leaks
-#include "AEEngine.h"
+#include "drawWallsLevel1.hpp" 
 
 // ---------------------------------------------------------------------------
 // main
@@ -17,45 +17,37 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-
 	int gGameRunning = 1;
 
-	// Initialization of your own variables go here
-
-	// Using custom window procedure
 	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 60, false, NULL);
-
-	// Changing the window title
-	AESysSetWindowTitle("My New Demo!");
-
-	// reset the system modules
+	AESysSetWindowTitle("TeamProjTest");
 	AESysReset();
+	printf("Team project test\n");
 
-	printf("Hello World\n");
+	initMesh(); // Initializing the meshes for the map
+	loadTextures(); // Loading textures
+	initTransforms(); // Initializing the transforms
 
-	// Game Loop
 	while (gGameRunning)
 	{
-		// Informing the system about the loop's start
 		AESysFrameStart();
 
-		// Basic way to trigger exiting the application
-		// when ESCAPE is hit or when the window is closed
 		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
 			gGameRunning = 0;
 
-		// Your own update logic goes here
+		AEGfxSetBackgroundColor(0.1f, 0.1f, 0.1f);
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		AEGfxSetColorToMultiply(1.0f, 1.0f, 1.0f, 1.0f);
+		AEGfxSetColorToAdd(0.0f, 0.0f, 0.0f, 0.0f);
+		AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+		AEGfxSetTransparency(1.0f);
 
+		drawFloors(); // Draw the map floors within the game loop
+		drawCharacter(); // Draw the character within the game loop
 
-		// Your own rendering logic goes here
-
-
-		// Informing the system about the loop's end
 		AESysFrameEnd();
-
 	}
-
-
-	// free the system
+	AEGfxMeshFree(textureMesh);
+	unloadTextures();
 	AESysExit();
 }
