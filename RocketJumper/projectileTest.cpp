ad0a5a8b0942b37fd45436a4c0470fb6b2ab3244
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-01-28 07:41:06 +0800

    removed physicsengine header and cpp

--- RocketJumper/projectileTest.cpp
+++ RocketJumper/projectileTest.cpp
@@ -13,7 +13,6 @@ Technology is prohibited.
 */
 /* End Header **************************************************************************/
 #include <iostream>
-#include "physicsEngine.h"
 #include "ProjectileTest.h"
 #include "collision.h"
 #include "projectile.h"
@@ -82,7 +81,6 @@ void ProjectileTest_Load()
 
 void ProjectileTest_Initialize()
 {
-	physics::initPhysics();
 	printf("ProjectileTest_Initialize: Ready for testing!\n");
 }
 
@@ -111,7 +109,6 @@ void ProjectileTest_Update()
 	}
 	*/
 	
-	physics::physicsInput();
 	// Get mouse inputs
 	s32 mouseX, mouseY;
 	AEInputGetCursorPosition(&mouseX, &mouseY);
@@ -120,7 +117,6 @@ void ProjectileTest_Update()
 	f32 worldMouseX = static_cast<f32>(mouseX) - static_cast<f32>(screenWidth / 2);
 	f32 worldMouseY = static_cast<f32>(screenLength / 2) - static_cast<f32>(mouseY);
 
-	//physics::updatePhysics(testObjects[player]);
 	// ========== JETPACK MOVEMENT SYSTEM ==========
 	// Apply thrust when spacebar is pressed
 	movement::physicsInput(testObjects[player]);
