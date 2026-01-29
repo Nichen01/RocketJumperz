7b5d6a94d6066f4b267565e9f290a2419a58f699
joremeowfaso <c.joraye@digipen.edu>
2026-01-29 22:21:56 +0800

    made a transition for both game states

--- RocketJumper/GameStateList.h
+++ RocketJumper/GameStateList.h
@@ -4,6 +4,7 @@
 enum GS_STATES
 {
 	GS_LEVEL1 = 0,
+	GS_LEVEL2,
 
 	GS_QUIT,
 	GS_RESTART
--- RocketJumper/GameStateManager.cpp
+++ RocketJumper/GameStateManager.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
-#include "mapEditor.h"
+#include "mapEditor_Level1.h"
+#include "mapEditor_Level2.h"
 #include "GameStateManager.h"
 
 // Tracks current, previous and next game states
@@ -16,10 +17,10 @@ void GSM_Initialize(int startingState)
 
 // Updates the Game State Manager and assigns function pointers based on current state
 void GSM_Update()
-{	
+{
 	switch (current)
 	{
-	case GS_LEVEL1:		
+	case GS_LEVEL1:
 		fpLoad = Level1_Load;
 		fpInitialize = Level1_Initialize;
 		fpUpdate = Level1_Update;
@@ -27,11 +28,16 @@ void GSM_Update()
 		fpFree = Level1_Free;
 		fpUnload = Level1_Unload;
 		break;
-	case GS_RESTART:
+	case GS_LEVEL2:
+		fpLoad = Level2_Load;
+		fpInitialize = Level2_Initialize;
+		fpUpdate = Level2_Update;
+		fpDraw = Level2_Draw;
+		fpFree = Level2_Free;
+		fpUnload = Level2_Unload;
 		break;
-	case GS_QUIT:
-		break;
-	default:
-		break;
+	case GS_RESTART:break;
+	case GS_QUIT:break;
+	default: break;
 	}
 }
