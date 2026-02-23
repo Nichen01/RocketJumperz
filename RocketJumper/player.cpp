49bb9faffc5aa92362a757da9183a667a335b3f4
i.chong <i.chong@digipen.edu>
2026-02-23 09:26:15 +0800

    added new player parameters

--- RocketJumper/player.cpp
+++ RocketJumper/player.cpp
@@ -2,19 +2,4 @@
 #include "collision.h"
 
 
-	void calcCorners(int map[], int mapX, objectsquares* player1) {
-
-		//determines coordinates in 2D array
-		
-		player1->leftX = (int)((player1->xPos - (player1->xScale / 2.0))+800)/ 100;
-		player1->rightX = (int)((player1->xPos + (player1->xScale / 2.0)) + 800) / 100;
-		player1->topY = -((int)((player1->yPos + (player1->yScale / 2.0)) - 450) / 100);
-		player1->bottomY = -((int)((player1->yPos - (player1->yScale / 2.0)) - 450) / 100);
-		
-
-		//determine data in corners, top bottom left right
-		player1->TR = map[player1->topY * mapX + player1->rightX];
-		player1->TL = map[player1->topY * mapX + player1->leftX];
-		player1->BR = map[player1->bottomY * mapX + player1->rightX];
-		player1->BL = map[player1->bottomY * mapX + player1->leftX];
-	}
+	
