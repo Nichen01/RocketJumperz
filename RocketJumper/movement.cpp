a97bb461504bf9bb2c771c18020fc4749dbc4c5f
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-03-01 09:31:49 +0800

    test 2.0 normal movement

--- RocketJumper/movement.cpp
+++ RocketJumper/movement.cpp
@@ -56,8 +56,8 @@ namespace movement {
         {
             // Calculate direction vector from player to mouse
             getMouse(player);
-            player.velocityX -= directionVector.x * ABSOLUTE_RECOIL;
-            player.velocityY -= directionVector.y * ABSOLUTE_RECOIL;
+            player.velocityX -= directionVector.x * ABSOLUTE_RECOIL * 0.5;
+            player.velocityY -= directionVector.y * ABSOLUTE_RECOIL * 1.7;
             printf("Bullet fired! Velocity: (%.2f, %.2f)\n", player.velocityX, player.velocityY);
             bulletCount -= 1;
         }
