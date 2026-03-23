4e4b0beed5ac9ade6ba3e5ad5b7ec906d58f22f8
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-03-23 10:23:53 +0800

    merged ivan into jeremiah
    Victory screen
    death screen
    main menu

--- RocketJumper/Main.cpp
+++ RocketJumper/Main.cpp
@@ -41,7 +41,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	AESysReset();
 	printf("Team project test\n");
 
-	GSM_Initialize(GS_TUTORIAL);
+	GSM_Initialize(GS_MAINMENU);
 
 	while (current != GS_QUIT)
 	{
