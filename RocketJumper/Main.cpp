7f86f7d77e3f5624bb9ea0da8a0a1292812fc7fa
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-04-02 12:14:51 +0800

    tweak trapsuction, removed warnings

--- RocketJumper/Main.cpp
+++ RocketJumper/Main.cpp
@@ -30,7 +30,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	{
 		return 0;
 	}
-	//AESysSetFullScreen(1);
+	AESysSetFullScreen(1);
 	// Using custom window procedure
 	//int gGameRunning = 1;
 
@@ -41,7 +41,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	AESysReset();
 	printf("Team project test\n");
 	
-	GSM_Initialize(GS_VICTORY);
+	GSM_Initialize(GS_MAINMENU);
 
 	while (current != GS_QUIT)
 	{
