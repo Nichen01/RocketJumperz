8e8e6f8abff9b4e4d16bbbd9d3692a3e1cb6ba49
jeremiah-he <jeremiahshuo.he@digipen.edu>
2026-04-04 22:37:51 +0800

    fullscreen confirmation

--- RocketJumper/Confirmation.h
+++ RocketJumper/Confirmation.h
@@ -6,5 +6,5 @@
 void Confirmation_Init(MenuButton& yesButton, MenuButton& noButton);
 void Confirmation_Update(MenuButton& yesButton, MenuButton& noButton, s8& leave);
 void Confirmation_Draw(s8 font, MenuButton& yesButton, MenuButton& noButton);
-
+void fsDraw(s8 font, MenuButton& yesButton, MenuButton& noButton);
 
